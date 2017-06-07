ea3376266f951265f76c6b244ffccbef93fa6c64
Jan Dietrich <dietrich@pik-potsdam.de>
2017-06-07 18:30:03 +0200

    added function collectFunctions to automatically detect available checks

---
+++ R/collectFunctions.R
@@ -0,0 +1,41 @@
+#' collectFunctions
+#'
+#' Collects functions which follow a given name pattern
+#' and compares their arguments against a list of allowed arguments
+#'
+#'
+#' @param pattern Name pattern the function name should match
+#' @param globalenv Boolean deciding whether functions in the global environment should be considered
+#' or not.
+#' @param allowed_args Vector of allowed arguments. If a function contains an argument not listed here it will be
+#' ignored and a warning will be returned.
+#' @return a character vector of function calls fulfilling all requirements
+#' @author Jan Philipp Dietrich
+#' @seealso \code{\link{iamCheck}}
+#' @examples
+#'
+#' collectFunctions()
+#'
+#' @importFrom methods formalArgs
+#' @export
+
+collectFunctions <- function(pattern="^check", globalenv=FALSE, allowed_args=c("x","mx","cfg")) {
+
+  funcs <- paste0("iamc:::",ls(getNamespace("iamc"), pattern=pattern))
+  if(globalenv) {
+    funcs <- c(funcs, ls(globalenv(),pattern=pattern))
+  }
+
+  checks <- NULL
+
+  for(func in funcs) {
+    args <- formalArgs(eval(parse(text = func)))
+    if(all(args %in% allowed_args)) {
+      checks <- c(checks, paste0(func,"(",paste(args,collapse=", "),")"))
+    } else {
+      invalid_args <- args[!(args %in% allowed_args)]
+      warning("Function \"",func,"\" ignored because of invalid arguments (",paste(invalid_args, collapse=", "),")")
+    }
+  }
+  return(checks)
+}
