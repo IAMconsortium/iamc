cf217a358da47d920bd0973beb2c2399b6dcca41
Cornelia Auer <coauer@pik-potsdam.de>
2017-07-03 18:14:47 +0200

    pre-checks: modified return messages

--- R/preCheckIllegalVariables.R
+++ R/preCheckIllegalVariables.R
@@ -1,7 +1,7 @@
 
 #check variable names that are in x but not in template cfg and need to be filtered
 preCheckIllegalVariables <- function(x, intersectVariables) {
-    return(list(message="data contains %# non-standard variables",
+    return(list(message="your data contains %# non-standard variables, which will be deleted",
                 failed=setdiff(x$variable,intersectVariables)))
 
 }
--- R/preCheckMissingVariables.R
+++ R/preCheckMissingVariables.R
@@ -1,7 +1,7 @@
 
 #check variable names that are not in x but maybe important
 preCheckMissingVariables <- function(cfg, intersectVariables) {
-    return(list(message="data missing following %# standard variables ",
+    return(list(message="your data is missing the following %# standard variables ",
                 failed=setdiff(cfg$variable,intersectVariables)))
 
 }
